0fbcb80bdd807a18506b0919081763cca467343b
hualin wu <whlkw@hotmail.com>
2018-04-08 20:20:25 +0800

    add testing codes for python function

--- function.py
+++ function.py
@@ -22,3 +22,22 @@ def changeList(myList):
 
 changeList(myList)
 print('the outer int is', myList)
+
+#An arbitrary parameter number can be accomplished in Python with so-called tuple references.
+#An asterisk "*" is used in front of the last parameter name to denote it as a tuple reference.
+def arithmetic_mean(first, *values):
+    """ This function calculates the arithmetic mean of a non-empty
+        arbitrary number of numerical values """
+
+    return (first + sum(values)) / (1 + len(values))
+
+print(arithmetic_mean(45,32,89,78))
+print(arithmetic_mean(8989.8,78787.78,3453,78778.73))
+print(arithmetic_mean(45,32))
+print(arithmetic_mean(45))
+
+#It is also possible to pass an arbitrary number of keyword parameters to a function.
+#To this purpose, we have to use the double asterisk "**"
+def funcUnpacKeywordArgs(**kwargs):
+    print(kwargs)
+funcUnpacKeywordArgs(de="German",en="English",fr="French")
